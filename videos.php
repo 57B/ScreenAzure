045d8b068669047d4301ead6afd06c541cf84f28
57B <67490664+57B@users.noreply.github.com>
2022-05-01 14:26:53 -0400

    Add files via upload

---
+++ videos.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/1999/REC-html401-19991224/loose.dtd">
+<html><!-- machid: 117 --><head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"><script src="videos_files/analytics.js" type="text/javascript"></script>
+<script type="text/javascript">window.addEventListener('DOMContentLoaded',function(){var v=archive_analytics.values;v.service='wb';v.server_name='wwwb-app200.us.archive.org';v.server_ms=271;archive_analytics.send_pageview({});});</script>
+<script type="text/javascript" src="videos_files/bundle-playback.js" charset="utf-8"></script>
+<script type="text/javascript" src="videos_files/wombat.js" charset="utf-8"></script>
+<script type="text/javascript">
+  __wm.init("http://web.archive.org/web");
+  __wm.wombat("http://www.youtube.com/login?next=/","20070128181526","http://web.archive.org/","web","/_static/",
+	      "1170008126");
+</script>
+<link rel="stylesheet" type="text/css" href="videos_files/banner-styles.css">
+<link rel="stylesheet" type="text/css" href="videos_files/iconochive.css">
+<!-- Start Wayback Rewrite JS Include -->
+<script type="text/javascript" src="videos_files/jwplayer.js" charset="utf-8"></script>
+<script type="text/javascript" src="videos_files/video-embed-rewriter.js" charset="utf-8"></script>
+<script type="text/javascript">
+_wmVideos_.init("/web");
+</script>
+<!-- End Wayback Rewrite JS Include -->
+
+	
+	<title>ScreenAzure - Screencast Yourself.</title>
+
+	<link rel="stylesheet" href="videos_files/styles_yts1164775696.css" type="text/css">
+	<link rel="stylesheet" href="videos_files/base_yts1169765684.css" type="text/css">
+	<link rel="icon" href="http://web.archive.org/web/20070128181526im_/http://www.youtube.com/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="http://web.archive.org/web/20070128181526im_/http://www.youtube.com/favicon.ico" type="image/x-icon">
+	
+	
+	<meta name="keywords" content="video,sharing,camera phone,video phone">
+
+	<link rel="alternate" title="YouTube - [RSS]" href="http://web.archive.org/web/20070128181526/http://www.youtube.com/rssls">
+
+	<script type="text/javascript" src="videos_files/ui_yts1169707248.js"></script>
+	<script type="text/javascript" src="videos_files/AJAX_yts1161839869.js"></script>
+	<script type="text/javascript" src="videos_files/watch_queue_yts1169707248.js"></script>
+	<script language="javascript" type="text/javascript">
+		onLoadFunctionList = new Array();
+		function performOnLoadFunctions()
+		{
+			for (var i in onLoadFunctionList)
+			{
+				onLoadFunctionList[i]();
+			}
+		}
+	</script>
+	
+	<script language="javascript" type="text/javascript"> function _hbLink (a,b) { return false; } </script>
+
+<script>
+function redirectSignupPage() {
+	if(document.signupForm.signup_type.value=='d') {
+		window.location='/signup?signup_type=d';
+	} 
+	else if(document.signupForm.signup_type.value=='m') {
+		window.location='/signup?signup_type=m';
+	} 
+	else if(document.signupForm.signup_type.value=='c') {
+		window.location='/signup?signup_type=c';
+	} 
+	else if(document.signupForm.signup_type.value=='g') {
+		window.location='/signup?signup_type=g';
+	}
+	else if(document.signupForm.signup_type.value=='u') {
+		window.location='/signup';
+	} 
+	else {
+		window.location='/signup';
+	}
+}
+</script>
+
+</head>
+
+
+<body onload="performOnLoadFunctions();"><!-- BEGIN WAYBACK TOOLBAR INSERT -->
+<style type="text/css">
+body {
+  margin-top:0 !important;
+  padding-top:0 !important;
+  /*min-width:800px !important;*/
+}
+</style>
+<script>__wm.rw(0);</script>
+<div id="wm-ipp-base" style="display: block; direction: ltr; height: 1px;" lang="en">
+</div><div id="wm-ipp-print">The Wayback Machine - http://web.archive.org/web/20070128181526/http://www.youtube.com/login?next=/</div>
+<script type="text/javascript">
+__wm.bt(675,27,25,2,"web","http://www.youtube.com/login?next=/","20070128181526",1996,"/_static/",["/_static/css/banner-styles.css?v=fantwOh2","/_static/css/iconochive.css?v=qtvMKcIJ"], false);
+  __wm.rw(1);
+</script>
+<!-- END WAYBACK TOOLBAR INSERT -->
+
+<div id="baseDiv">	
+		<div id="logoTagDiv">
+	<a name="&amp;lid=Logo&amp;lpos=GlobalNav" href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php"><img src="videos_files/pic_youtubelogo_123x63.gif" alt="Home" onmouseover="showDiv('logoHomeTip');" onmouseout="hideDiv('logoHomeTip');" width="123" height="63" border="0"></a>
+	</div>
+	<div id="logoHomeTip" style="display: none;">Home</div>
+
+	<div id="utilDiv">
+	<script type="text/javascript">var quicklist_count=0;</script>
+	
+		<b><a onclick="_hbLink('SignUp','UtilityLinks');" href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/signup.php">Sign Up</a></b>
+		<span class="utilDelim">|</span>
+		<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">My Account</a>
+		<span class="utilDelim">|</span>
+		<a onclick="_hbLink('ViewingHistory','UtilityLinks');" href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">History</a> 
+		<span class="utilDelim">|</span>
+		<a onclick="_hbLink('QuickList','UtilityLinks');" href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">QuickList</a>
+		(<span id="quicklist_numb"><a href="http://web.archive.org/web/20070128181526/http://www.youtube.com/watch_queue?all"><script type="text/javascript">var quicklist_count=0;document.write(quicklist_count);</script>000000</a></span>)
+
+		<span class="utilDelim">|</span>
+		
+<a onclick="_hbLink('LogIn','UtilityLinks');" href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/help.php">Help</a>
+		<span class="utilDelim">|</span>                        
+		<a onclick="_hbLink('LogIn','UtilityLinks');" href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/login.php">Log In</a>
+	<form name="logoutForm" method="post" action="/web/20070128181526/http://www.youtube.com/index">
+		<input type="hidden" name="action_logout" value="1">
+	</form>
+</div>
+
+	
+	<div id="searchDiv">
+		<form name="searchForm" id="searchForm" method="get" action="index.php">
+		<input tabindex="10000" type="text" name="search_query" maxlength="128" class="searchField">
+		&nbsp;
+		<input type="submit" name="search" value="Search">
+	</form>
+
+	</div>
+		
+	<div id="gNavDiv">
+		
+		
+		<div id="upload"><a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/notice.php"><img src="videos_files/pic_upload_130x28.gif" alt="upload" width="130" height="28" border="0"></a></div>
+
+		<div class="tab">
+			<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/community.php"><img src="videos_files/tab_community_118x28.gif" alt="community" width="118" height="28" border="0"></a></div>
+		<div class="tab">
+			<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/channels.php"><img src="videos_files/tab_channels_118x28.gif" alt="channels" width="118" height="28" border="0"></a></div>
+		<div class="tab">
+			<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/categories.php"><img src="videos_files/tab_categories_118x28.gif" alt="categories" width="118" height="28" border="0"></a></div>
+		<div class="tab">
+			<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/videos.php"><img src="videos_files/tab_videos_118x28.gif" alt="videos" width="118" height="28" border="0"></a></div>
+	</div>
+<!-- end gNavDiv -->
+		<div id="gNavBottom">&nbsp;</div>
+
+
+	
+	
+	
+	
+
+
+
+
+
+
+
+<h1>Feature Not Avaible As Of This Time!</h1>
+
+
+
+
+
+<script type="text/javascript">
+document.loginForm.username.focus();
+</script>
+
+		<div class="spacer">&nbsp;</div>
+	<div id="footerDiv">
+		<div id="footerContent">
+		<div id="footerSearch">
+			<form name="searchFormFooter" id="searchFormFooter" method="get" action="/web/20070128181526/http://www.youtube.com/results">
+			<input type="text" name="search_query" maxlength="128" class="searchField">
+			&nbsp;
+			<input type="submit" name="search" value="Search">
+			</form>
+		</div> <!-- end footerSearch -->
+
+	
+		<div id="footerLinks">
+			
+			<table width="100%" cellspacing="0" cellpadding="0" border="0" align="center"><tbody><tr valign="top">
+			
+			<td>
+			<div class="footColumnLeft">
+				<div class="footLabel">Your Account</div>
+				<div class="footValues">
+					<div class="column">
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">Videos</a><br>
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">Favorites</a><br>
+					</div>
+					<div class="column">
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">Playlists</a><br>
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">Inbox</a><br>
+					</div>
+					<div class="column">
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/index.php">Subscriptions</a><br>
+					<br>
+					</div>
+				</div>
+			</div>
+			</td>
+			
+			
+			
+			
+			<td>
+			<div class="footColumnRight">
+				<div class="footLabel">ScreenAzure</div>
+				<div class="footValues">
+					<div class="column">
+					<br>
+					<br>
+					</div>
+					<div class="column">
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/terms.php">Terms of Use</a><br>
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/privacy.php">Privacy Policy</a><br>
+					</div>
+					<div class="column">
+					<br>
+					<a href="file:///Users/issacmcbeigh/Desktop/ScreenAzure/contact.php">Contact</a><br>
+					</div>
+					<div class="column">
+					<br>
+					<br>
+					</div>
+				</div>
+			</div>
+			</td>
+			
+			</tr></tbody></table>
+			
+		</div> <!-- end footerLinks -->
+		</div> <!-- end footerContent -->
+
+
+		<div id="footerCopyright">
+			Copyright © 2022 ScreenAzure.
+
+		</div> <!-- end footerCopyright -->
+		
+	</div> <!-- end footerDiv -->
+
+</div> <!-- end baseDiv -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body></html>
+<!--
+     FILE ARCHIVED ON 18:15:26 Jan 28, 2007 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 04:41:17 May 01, 2022.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+-->
+<!--
+playback timings (ms):
+  captures_list: 175.836
+  exclusion.robots: 0.17
+  exclusion.robots.policy: 0.164
+  cdx.remote: 0.057
+  esindex: 0.007
+  LoadShardBlock: 137.412 (3)
+  PetaboxLoader3.datanode: 67.996 (4)
+  CDXLines.iter: 16.635 (3)
+  load_resource: 88.106
+  PetaboxLoader3.resolve: 39.751
+-->
